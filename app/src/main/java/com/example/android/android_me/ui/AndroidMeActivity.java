3c5c42284df65963d91da4556fefcd99ba085b60
sdtareq <sdtareq@gmail.com>
2017-11-29 21:27:20 +0600

    Create My First Fragment

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,9 +16,10 @@
 
 package com.example.android.android_me.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.example.android.android_me.ui.BodyPartFragment;
 import com.example.android.android_me.R;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
@@ -40,5 +41,9 @@ public class AndroidMeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_android_me);
 
         // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.head_container, bodyPartFragment)
+                .commit();
     }
 }
